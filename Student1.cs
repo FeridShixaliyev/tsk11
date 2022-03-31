b46ed9b09abf99941fb8e3eff872ef9462de3ab9
FeridShixaliyev <tu7b9ed4n@code.edu.az>
2022-03-31 12:05:02 +0400

    task

---
+++ Student1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp37
+{
+    class Student
+    {
+        public string FullName { get; set; }
+        public int NO { get; private set; }
+        
+        public Student()
+        {
+            NO++;
+        }
+             
+    }
+}
