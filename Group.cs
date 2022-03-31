2568e34f150c76fcbb952a88df22d828a7d756b8
FeridShixaliyev <tu7b9ed4n@code.edu.az>
2022-03-31 12:04:45 +0400

    task

---
+++ Group.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp37
+{
+    enum Type
+    {
+        Programming, 
+        Design,
+        System
+    }
+    class Group
+    {
+        public string No { get; set; }
+        public int _no=100;
+        Student[] students;
+        public Type type { get; set; }
+
+        public Group()
+        {
+            _no++;    
+        }
+        
+
+        
+    }
+}
